0634f6573a2add042c5e5d72fef3b8b26b72d238
shabuhabs <shabuhabs@gmail.com>
2023-05-02 12:57:10 -0400

    added junit test and maven CI-CD

---
+++ shop/src/test/java/com/shabushabu/javashop/shop/ShopTest.java
@@ -0,0 +1,32 @@
+
+package com.shabushabu.javashop.shop;
+
+//import org.junit.jupiter.api.Test;
+// import org.junit.jupiter.api.Assertions.assertEquals;
+
+
+
+public class ShopTest {
+
+   // @Before
+   public void setUp() throws Exception 
+   {
+   }
+
+   // @Test
+   public void test_25() throws Exception
+   {
+   //    assertEquals(25, 25);
+	   if ( 1 == 0 ) {
+		   throw new Exception("test_25_failed");
+	   }
+	  
+   }
+	
+   // @Test
+   public void  test_the_Shop() throws Exception
+   {
+   //    assertEquals(1, 1) ; // Main.main(null);
+	  // return 1;
+   }
+}
