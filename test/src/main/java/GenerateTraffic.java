4a3bb6112e014d2e1b11d6004b04f83197493c7f
shabuhabs <shabuhabs@gmail.com>
2023-07-30 22:03:32 +0000

    fixed conductors

--- test/src/main/java/GenerateTraffic.java
+++ test/src/main/java/GenerateTraffic.java
@@ -12,7 +12,7 @@ public class GenerateTraffic {
 	
 	public static void main(String[] args) {
                 try {	
-		  Thread.sleep(45000);
+		  Thread.sleep(120000);
 		}catch(Exception e){
 		}
 		  
